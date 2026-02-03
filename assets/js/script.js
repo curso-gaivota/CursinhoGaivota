99f77416cc7c0f3cdd52c55de09a5a3ad0d160e6
Estéfany <m.estefany.sena@gmail.com>
2026-02-03 02:43:02 -0300

    1-Html e Css base

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Aqui você pode adicionar interatividade, se quiser
+// Exemplo: scroll suave para o botão
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e){
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
