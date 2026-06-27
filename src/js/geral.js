598a98591503343b7646484b3cac0e6691304adc
Estéfany <m.estefany.sena@gmail.com>
2026-06-27 00:02:47 -0300

    16-Acrescentando a landing page

---
+++ src/js/geral.js
@@ -0,0 +1,16 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const elements = document.querySelectorAll(".reveal");
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("active");
+            }
+        });
+    });
+
+    elements.forEach(el => observer.observe(el));
+
+});
